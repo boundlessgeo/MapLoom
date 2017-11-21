87d7faa2c154c19357178ce26a00727963db2613
Yancy Matherne <ymatherne@boundlessgeo.com>
2017-11-21 15:29:06 -0600

    PSBEX-4: Add Layer Transparency Slider,
    Add a slider for transparency in the layer info section of the left menu.
    Does not appear for base layers.
    Transparency setting is saved in the map configuration and is used on map load.
    Does work for Heatmap layers.

--- src/common/layers/LayersDirective.js
+++ src/common/layers/LayersDirective.js
@@ -155,6 +155,14 @@
             scope.getLayerAttributeVisibility = function(layer) {
               $rootScope.$broadcast('getLayerAttributeVisibility', layer);
             };
+
+            scope.initializeOpacity = function(layer) {
+              // Heatmap layers do not get a config object with a default opacity.
+              // Let's initialize it here so that everything else can work properly.
+              if (_.isNil(_.get(layer.get('metadata'), 'config.opacity'))) {
+                _.set(layer.get('metadata'), 'config.opacity', layer.getOpacity());
+              }
+            };
           }
         };
       }
--- src/common/map/MapService.js
+++ src/common/map/MapService.js
@@ -1046,6 +1046,10 @@
           meta.layerOrder++;
         }
 
+        if (!_.isNil(meta.config.opacity)) {
+          layer.setOpacity(meta.config.opacity);
+        }
+
         var insertIndex = -1;
 
         for (var index = 0; index < mapLayers.length; index++) {
