2f608ab767c010f041a5cde6558bc1f84c924685
Clarence Davis <cldavisjr@gmail.com>
2018-03-15 13:53:38 -0500

    remove config check

--- src/common/layers/LayersDirective.js
+++ src/common/layers/LayersDirective.js
@@ -96,20 +96,18 @@
             };
 
             scope.saveLayerStyle = function(layer) {
-              if (configService.configuration.stylingEnabled) {
-                if (goog.isDefAndNotNull(layer.get('metadata').defaultStyle)) {
-                  var loading = layer.get('metadata').loadingStyle || true;
-                  if (goog.isDefAndNotNull(loading) && loading) {
-                    layer.get('metadata').loadingStyle = true;
-                    mapService.updateStyle(layer).then(function() {
-                      layer.get('metadata').loadingStyle = false;
-                    }, function() {
-                      layer.get('metadata').loadingStyle = false;
-                      dialogService.error($translate.instant('save_layer_style'),
-                          $translate.instant('style_layer_failed',
-                              { 'style_name': layer.get('metadata').defaultStyle.name}));
-                    });
-                  }
+              if (goog.isDefAndNotNull(layer.get('metadata').defaultStyle)) {
+                var loading = layer.get('metadata').loadingStyle || true;
+                if (goog.isDefAndNotNull(loading) && loading) {
+                  layer.get('metadata').loadingStyle = true;
+                  mapService.updateStyle(layer).then(function() {
+                    layer.get('metadata').loadingStyle = false;
+                  }, function() {
+                    layer.get('metadata').loadingStyle = false;
+                    dialogService.error($translate.instant('save_layer_style'),
+                        $translate.instant('style_layer_failed',
+                            { 'style_name': layer.get('metadata').defaultStyle.name}));
+                  });
                 }
               }
             };
